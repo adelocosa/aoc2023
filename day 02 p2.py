442540a7d1e076306c824e1d4dbefaa4d31aea85
adelocosa <jmquander@gmail.com>
2023-12-23 13:46:32 -0500

    day 2 p2

---
+++ day 02 p2.py
@@ -0,0 +1,48 @@
+from utils import *
+from pprint import pprint
+from typing import Sequence
+from collections import defaultdict
+
+test_input = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+
+
+def parse_games(input: Sequence[str]) -> list[list[dict[str, int]]]:
+    games = []
+    for line in input:
+        line = line.split(":")[1][1:]
+        steps = [x.replace(",", "").lstrip(" ") for x in line.split(";")]
+        game = []
+        for step_str in steps:
+            ball_counts = step_str.split()[::2]
+            ball_colors = step_str.split()[1::2]
+            step = {}
+            for i, color in enumerate(ball_colors):
+                step[color] = int(ball_counts[i])
+            game.append(step)
+        games.append(game)
+    return games
+
+
+def solve(test: bool) -> int:
+    if test:
+        input = test_input.split("\n")
+    else:
+        input = get_input(2)
+    games = parse_games(input)
+    total = 0
+    for game in games:
+        max_cubes: dict[str, int] = defaultdict(int)
+        for step in game:
+            for color, amount in step.items():
+                max_cubes[color] = max(amount, max_cubes[color])
+        power = max_cubes["green"] * max_cubes["red"] * max_cubes["blue"]
+        total += power
+    return total
+
+
+answer = solve(test=False)
+print(answer)
