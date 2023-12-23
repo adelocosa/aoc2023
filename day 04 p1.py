73e6879fd931634d7a744461a20a29bb2e63a7fe
adelocosa <jmquander@gmail.com>
2023-12-23 16:25:39 -0500

    day 4 p1

--- day 04 p1.py
+++ day 04 p1.py
@@ -1,6 +1,9 @@
 from utils import *
 from pprint import pprint
-from typing import Sequence
+from typing import Sequence, TypeAlias, Any
+import copy
+
+Card: TypeAlias = dict[str, Any]
 
 test_input = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
@@ -10,14 +13,25 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
 
 
-def parse_input(input: Sequence[str]) -> list[tuple[list[int], list[int]]]:
+def parse_input(input: Sequence[str]) -> list[Card]:
     cards = []
     for line in input:
+        card: dict[str, Any] = {"count": 1}
         line = line.split(":")[1][1:].replace("  ", " ")
         winning_numbers_str, my_numbers_str = line.split("|")
-        winning_numbers = [int(x) for x in winning_numbers_str.strip(" ").split(" ")]
-        my_numbers = [int(x) for x in my_numbers_str.strip(" ").split(" ")]
-        cards.append((winning_numbers, my_numbers))
+        card["winning"] = [int(x) for x in winning_numbers_str.strip(" ").split(" ")]
+        card["mine"] = [int(x) for x in my_numbers_str.strip(" ").split(" ")]
+        cards.append(card)
+    return cards
+
+
+def iterate_cards(cards: list[Card]) -> list[Card]:
+    for i, card in enumerate(cards):
+        card_to_add = 1
+        for number in card["winning"]:
+            if number in card["mine"]:
+                cards[i + card_to_add]["count"] += card["count"]
+                card_to_add += 1
     return cards
 
 
@@ -27,17 +41,11 @@ def solve(test: bool) -> int:
     else:
         input = get_input(day=4)
     cards = parse_input(input)
-    total_points = 0
+    cards = iterate_cards(cards)
+    total_cards = 0
     for card in cards:
-        points = 0
-        for number in card[0]:
-            if number in card[1]:
-                if not points:
-                    points += 1
-                else:
-                    points *= 2
-        total_points += points
-    return total_points
+        total_cards += card["count"]
+    return total_cards
 
 
 answer = solve(test=False)
